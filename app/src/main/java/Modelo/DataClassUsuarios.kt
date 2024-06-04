dee4beddcff502a5c0f3c81710d67f355973f50b
Luis Escalante <20200602@ricaldone.edu.sv>
2024-06-04 15:06:29 -0600

    Avances CRUD

---
+++ app/src/main/java/Modelo/DataClassUsuarios.kt
@@ -0,0 +1,9 @@
+package Modelo
+
+class DataClassUsuarios (
+        val uuid : String,
+        var nombre_usuario : String,
+        var contrasena_usuario: String
+    )
+
+
--- app/src/main/java/luis/escalante/myapplication/MainActivity.kt
+++ app/src/main/java/luis/escalante/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package luis.escalante.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtContrasena = findViewById<EditText>(R.id.txtContrasena)
+
+
+        val btnRegistrar: Button = findViewById(R.id.btnRegistrar)
+
+        btnRegistrar.setOnClickListener {
+
+            val nombre = txtNombre.text.toString()
+            val contrasena = txtContrasena.text.toString()
+
+            if (nombre.isEmpty() || contrasena.isEmpty()) {
+
+                Toast.makeText(
+                    this,
+                    "Error, para acceder debes llenar todas las casillas.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+
+                Log.i("Test de credenciales", "Correo: $nombre y Contraseña: $contrasena")
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            }
         }
     }
 }
